cae3b97fd4ec0d03a3e0c70846d9a4e45f6e012f
조다은 <thisisdana49@gmail.com>
2025-02-21 15:22:14 +0900

    Initial Commit

---
+++ TamagotchiGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TamagotchiGame
+//
+//  Created by 조다은 on 2/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
